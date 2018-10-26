5ae99842ad2d7b7ae169c55f172862e0c635222c
Altehon <lord_deletlos@hotmail.es>
2018-10-26 18:48:24 +0200

    obtencion de la funcion de transferencia del sistema

--- Practica 0/Parte 2/Ejercicio2.m
+++ Practica 0/Parte 2/Ejercicio2.m
@@ -3,10 +3,11 @@ b = 0.1;
 K = 0.01;
 R = 1;
 L = 0.5;
+
 syms dwdt; 
 syms didt;
-syms w i;
-syms V k;
+syms w j i;
+syms V k n;
 syms w(n) i(n);
 f1 = J * dwdt + b * w - K * 1i;
 f2 = L * didt + R * 1i - V + K * w;
@@ -21,13 +22,23 @@ didtd = (i(n) - i(n-1))/Ts;
 f1d = subs(f1, [dwdt w 1i], [dwdtd wd id]);
 f2d = subs(f2, [didt w 1i], [didtd wd id]);
 disp('Las ecuaciones en tiempo discreto seran las siguientes: ');
-pretty(f1d);
-pretty(f2d);
+pretty(f1d==0);
+pretty(f2d==0);
 
-% Obtencion de las transformada z
-
-tf1d = ztrans(f1d);
-tf2d = ztrans(f2d);
-disp('Las ecuaciones en transformada z seran:');
+% Calculamos la transformada z externamente y la añdimos
+syms W I z 
+disp('Calculamos la transformada z y obtenemos el par de ecuaciones: ');
+tf1d = J * (W-W*z^-1)/Ts + b*W - K * I == 0;
+tf2d = L * (I-I*z^-1)/Ts + R*I - V + W == 0;
 pretty(tf1d);
 pretty(tf2d);
+t1  = solve(tf1d,I);
+t2 = subs(tf2d,I,t1);
+disp('Sustituimos la I en la segunda ecuacion: ');
+pretty(t2);
+omega = solve(t2,W);
+disp('Despejamos la W de la ecuacion resultante');
+pretty(W==omega);
+disp('Para obtener la ganancia W/V dividimos entre V la ecuacion: ');
+ganancia = omega/V;
+pretty(W/V==ganancia);
